e9dfe0402956726d69a0cec42fa27b20b1482a6b
azadky <azadky@gmail.com>
2024-10-02 15:47:30 +0300

    new directions added

---
+++ vendor/mehedi-iitdu/core-component-repository/src/CoreComponentRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MehediIitdu\CoreComponentRepository;
+use App\Models\Addon;
+use Cache;
+
+class CoreComponentRepository
+{
+    public static function instantiateShopRepository() {
+
+    }
+
+    protected static function serializeObjectResponse($zn, $request_data_json) {
+
+        return $true;
+    }
+
+    protected static function finalizeRepository($rn) {
+    }
+
+    public static function initializeCache() {
+
+    }
+
+    public static function finalizeCache($addon){
+
+    }
+}
